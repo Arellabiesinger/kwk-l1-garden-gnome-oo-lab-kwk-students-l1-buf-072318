b34e98e86a5fb3020751b8e7f55cf84c248d43cf
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-30 13:27:05 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -4,10 +4,10 @@
 class GardenGnome
  attr_accessor :name, :age, :gluten_allergy, :personality#, :hat_color
 
- def initialize # (hat_color)
-  @personality = "evil"
-  @hat_color = color
-end
+   def initialize # (hat_color)
+    @personality = "evil"
+    @hat_color = color
+  end
 end
 
 test_gnome = GardenGnome.new
