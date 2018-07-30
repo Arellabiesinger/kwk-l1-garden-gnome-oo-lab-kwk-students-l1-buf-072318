bd8c59366caa916862719d62fd8b8a8602661f06
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-30 14:34:17 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -4,15 +4,15 @@
 class GardenGnome
  attr_accessor :name, :age, :gluten_allergy, :personality, :hat_color
 
-   def initialize(hat_color = "red")
-    @personality = "evil"
-    @hat_color = hat_color
-   end
+    def initialize(hat_color = "red")
+      @personality = "evil"
+      @hat_color = hat_color
+    end
     def gnaw
-       "Gnawing on a tree!!!"
+      "Gnawing on a tree!!!"
     end
     def shouts
-      
+      ""
     end
 end
 
